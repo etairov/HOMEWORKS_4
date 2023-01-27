84e8fc6aa8415545066ec6c0b886e4a0c9eb8ff8
e.tairov <ev.tairov@yandex.ru>
2023-01-27 20:07:48 +0700

    Home4_01

--- Home4_Task_01/Program.cs
+++ Home4_Task_01/Program.cs
@@ -4,3 +4,33 @@
 //2, 4 -> 16
 //---------------------
 
+int GetNumbers(string message)
+{
+    int result1 = 0;
+    int result2 = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result1))
+        {
+            break;
+        }
+        else Console.WriteLine("Ввели не число. Повторите ввод");
+
+        if (int.TryParse(Console.ReadLine(), out result2))
+        {
+            break;
+        }
+        else Console.WriteLine("Ввели не число. Повторите ввод");
+    }
+    return result1;
+    return result2;
+}
+
+int number1 = GetNumbers("Введите число A: ");
+int number2 = GetNumbers("Введите число B: ");
+
+double printDegree = Math.Pow(number1, number2);
+Console.WriteLine($"{number1}, {number2} --> {printDegree}");
