900585629c4b809244d43e9a31f84b04497de27c
e.tairov <ev.tairov@yandex.ru>
2023-01-29 03:13:41 +0700

    4_02

--- Home4_Task_02/Program.cs
+++ Home4_Task_02/Program.cs
@@ -5,3 +5,38 @@
 //9012 -> 12
 //--------------------
 
+int GetNumbers(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else Console.WriteLine("Ввели не число. Повторите ввод");
+    }
+    return result;
+}
+
+int numberGet = GetNumbers("Введите любое число: ");
+
+int SummOfDigits(int number)
+{
+int N = Math.Abs(numberGet);
+int sum = 0;
+int i = 0;
+while (N > 0)
+{
+    i++;
+    sum += N % 10;
+    N /= 10;
+}
+    return sum;
+}
+
+int summDigit = SummOfDigits(numberGet);
+Console.WriteLine($"{numberGet} --> {summDigit}");
