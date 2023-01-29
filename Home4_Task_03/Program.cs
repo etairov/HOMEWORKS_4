b5577e14708da74d1b22fa3dce3133fab6f4239d
e.tairov <ev.tairov@yandex.ru>
2023-01-29 18:27:59 +0700

    4_03

--- Home4_Task_03/Program.cs
+++ Home4_Task_03/Program.cs
@@ -4,3 +4,30 @@
 //6, 1, 33 -> [6, 1, 33]
 //---------------------------
 
+int[] GetRandomArray()
+{
+    int[] array = new int[8];
+    Random randomNumber = new Random();
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        array[i] = randomNumber.Next(-11, 11);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    Console.Write('[');
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length-1]}]");
+}
+
+int[] randomArray = GetRandomArray();
+
+Console.WriteLine($"Произвольно заданный массив чисел:");
+printArray(randomArray);
+
+
